7a0f199205a394bac52c6ff1642e17b295224be5
Eugen <krizhik@gmail.com>
2022-07-31 20:16:26 +0300

    products add, layout component

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { client } from '../lib/client';
 import { HeroBanner, FooterBanner, Product } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log('bannerData', bannerData)}
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => (
-          <Product />
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
         ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
